277de35aa80740f8cf53b3876749bbd51042ed66
seafarer <colin@studiotaraz.com>
2022-09-02 14:38:50 -0600

    change link to href

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,9 @@
-import Link from "next/link";
 
 export default function Footer() {
   return (
     <footer className="bg-indigo-900 p-4 flex justify-between">
       <p className="text-xs text-slate-400">Powered by Next.js | Search by Algolia | Data from OpenSea | Hosted on Vercel | CMS from Cosmic</p>
-      <p className="text-xs text-slate-400">Built by <Link href="https://www.highfidelity.dev" target="_blank" rel="noreferrer">High Fidelity</Link></p>
+      <p className="text-xs text-slate-400">Built by <a href="https://www.highfidelity.dev" target="_blank" rel="noreferrer">High Fidelity</a></p>
     </footer>
   )
 }
