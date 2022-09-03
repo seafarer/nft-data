57865d63d4fb59c65e8507bcb4503ebd795e2bbf
seafarer <colin@studiotaraz.com>
2022-09-03 12:58:27 -0600

    title fix

--- components/navbar.js
+++ components/navbar.js
@@ -19,7 +19,7 @@ export function Navbar() {
     <header className="border-t-4 border-indigo-400 bg-gradient-to-r from-indigo-900 to-blue-900 py-2 px-3 flex justify-between align-center">
       <h1 className="text-gray-100 font-bold text-xl">
         <Link href="/">
-          NFT DATA
+          NFT LOOKUP
         </Link>
       </h1>
       <h2 className="text-indigo-300 text-base hidden md:block">Quick stats for your favorite NFT collections on the Ethereum blockchain</h2>
