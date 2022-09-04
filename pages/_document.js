a27e21289a13fe239a31a1dbec5e7bcb5d331bf0
seafarer <colin@studiotaraz.com>
2022-09-04 10:03:56 -0600

    fix preconnect

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ class MyDocument extends Document {
             href="https://use.typekit.net/ftk1knh.css"
             rel="stylesheet"
           />
-          <link crossOrigin href={`https://${process.env.NEXT_PUBLIC_ALGOLIA_APPID}-dsn.algolia.net`} rel="preconnect"/>
+          <link crossOrigin="use-credentials" href={`https://${process.env.NEXT_PUBLIC_ALGOLIA_APPID}-dsn.algolia.net`} rel="preconnect"/>
         </Head>
         <body>
           <Main />
